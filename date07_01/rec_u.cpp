dbed34ba20f4b2e992f534c8a5d68f23988e909e
ayush8006 <ayushagrawal8006@gmail.com>
2024-01-08 18:39:20 +0530

    knapsack

---
+++ date07_01/rec_u.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+
+    int n,w;
+    cin>>n>>w;
+
+    vector<pair<int,int>>a(n);
+
+     for(int i=0;i<n;i++){
+
+        cin>>a[i].first>>a[i].second;
+
+
+    }
+    
+
+    
+
+}
+
+int main() {
+   int t=1;
+   //cin>>t;
+   while(t--){
+    solve();
+   }
+    return 0;
+}
